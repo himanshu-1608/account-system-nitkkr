2eab7755e5fb93dd7f904b64286da810183dc03a
Himanshu Yadav <ydv1608@gmail.com>
2021-08-29 21:08:29 +0530

    Utilized VendorService to check JDBC Working

--- src/account/system/AccountSystem.java
+++ src/account/system/AccountSystem.java
@@ -1,10 +1,16 @@
 package account.system;
 
+import account.system.dao.VendorService;
+import account.system.model.Vendor;
+
 public class AccountSystem {
 
     public static void main(String[] args) {
         try {
             System.out.println("Hello World");
+            Vendor v = new VendorService().getVendorByName("Vendor 2");
+            System.out.println("Found a vendor with id: " + v.getContractStartDate());
+            System.out.println("Operation Done");
         } catch (Exception ex) {
             System.out.println(ex);
         }
