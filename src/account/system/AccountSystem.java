9ddea5dd006a3bbf549663c9634f97c95cc764e7
Himanshu Yadav <ydv1608@gmail.com>
2021-08-29 20:43:28 +0530

    Initial Hello World Commit

---
+++ src/account/system/AccountSystem.java
@@ -0,0 +1,12 @@
+package account.system;
+
+public class AccountSystem {
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("Hello World");
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }    
+}
